b500e9052d00c002d6648c49a1411f2c15c6b0a4
Aleksandr Omelucik <bladewizard@mail.ru>
2025-07-12 19:01:02 +0300

    add storage

---
+++ docs/js/storage/storage.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC0onb9HLfNMSfk691vG53jhVcXo0l1SHI",
+  authDomain: "i-shop-31658.firebaseapp.com",
+  projectId: "i-shop-31658",
+  storageBucket: "i-shop-31658.firebasestorage.app",
+  messagingSenderId: "159294792751",
+  appId: "1:159294792751:web:a6bd251f3606c01f4a52da"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
